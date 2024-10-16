22a331d541784604f9b1697490844fed6a2dc91f
duncan <costes.duncan@gmail.com>
2024-10-16 11:28:16 +0200

    ajout logique json + call api

--- src/app/weather-group/weather-group.component.ts
+++ src/app/weather-group/weather-group.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit , inject } from "@angular/core";
 import { CITYLIST } from "../city-mock";
+import { City } from "../city";
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
     selector: 'app-weather-group',
@@ -8,8 +11,35 @@ import { CITYLIST } from "../city-mock";
 
 export class WeatherGroupComponent implements OnInit {
     constructor() {}
+    id: number= 3
+    cityList: City[] = CITYLIST
+    actualCityList: City[] = []
+    data:any
+
+    private http = inject(HttpClient);
 
     ngOnInit() {
         console.log(CITYLIST);
+        this.getWeather(this.id)
+        this.apiCall()
     }
+
+
+    getWeather(id: number) {
+        this.id = id
+        for (let i = this.id*50; i < (this.id*50)+50; i++) {
+            this.actualCityList.push(this.cityList[i]);
+        }
+    }
+
+    apiCall() {
+        console.log("apiCall");
+        this.http.get("https://jsonplaceholder.typicode.com/todos/1")
+            .subscribe((data:any )=> {
+                this.data = data
+                console.table(data);
+            });
+        console.log(this.data);
+    }
+
 }
