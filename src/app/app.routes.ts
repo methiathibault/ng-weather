4b21924aa130ebfb99194c4792071c47964cc43d
methiathibault <methiathibault@gmail.com>
2024-10-16 14:08:53 +0200

    starting weather component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { WeatherComponent } from './weather/weather.component';
 export const routes: Routes = [
     {path: 'weather-group', component: WeatherGroupComponent},
     {path: 'weather', component: WeatherComponent},
-    {path: '', redirectTo: '/weather-group', pathMatch: 'full'},
+    {path: '', redirectTo: 'weather', pathMatch: 'full'},
 ];
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,17 +1,41 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { City } from "../city";
+import { CITYLIST } from "../city-mock";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class WeatherService {
-    constructor() {}
-
-    getWeather(city_id: string) {
-        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_id}&appid=${environment.apiKey}`)
-            .then((response) => response.json())
-            .then((data) => data);
-        }
-    
+    httpclient = inject(HttpClient);
+    cityList: City[]
+    constructor() {
+        this.cityList = CITYLIST
     }
+
+    getWeatherFromId(city_id: string) {
+        return this.httpclient.get(`https://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${environment.apiKey}&units=metric`);
+    }
+
+    getIdFromCityName(city_name: string): number | null {
+        city_name = city_name.toLowerCase();
+        let city = this.cityList.find(city => city.name.toLowerCase() === city_name);
+        if (city) {
+            return city.id;
+        }
+        console.error(`City not found for ${city_name}`);
+        return null;
+    }
+    
+    getWeatherFromCityName(city_name: string): any | null {
+        let id = this.getIdFromCityName(city_name);
+        if (id) {
+            return this.getWeatherFromId(id.toString());
+        }
+        console.error(`City not found for ${city_name}`);
+        return null;
+    }
+    
+}
